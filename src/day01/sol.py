e398d9907aa42df90e14df7d4a9516d7de504a2a
nishanthegde <nishant.arun.hegde@gmail.com>
2025-09-19 18:51:00 -0700

    Add part 2 to day01 solution

--- src/day01/sol.py
+++ src/day01/sol.py
@@ -9,18 +9,26 @@ def parse_input(file):
 
 
 def calc_part1(left, right):
-    distance = [abs(a - b) for a, b in zip(left, right)]
+    return sum(abs(a - b) for a, b in zip(left, right))
 
-    return sum(distance)
+
+def calc_part2(left, right):
+    similarity = 0
+    for num in left:
+        similarity += num * right.count(num)
+
+    return similarity
 
 
 def main():
     sample_input_path = "../../inputs/day01_sample.txt"
     input_path = "../../inputs/day01.txt"
+    # left, right = parse_input(sample_input_path)
     left, right = parse_input(input_path)
 
     distance = calc_part1(left, right)
-    print(distance)
+    # print(distance)
+    print(calc_part2(left, right))
 
 
 if __name__ == "__main__":
